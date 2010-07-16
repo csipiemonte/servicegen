d5c55f5c266abb2b9e2299319076420d93bdbfd6
ANTONACI Mauro 869 <mauro.antonaci@csi.it>
2010-07-16 13:28:01 +0000

    STDMDD-532: prima impl funzionate tracking
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/servicegen/trunk@1758 be04b910-fd5c-0410-891f-bca81b977169

---
+++ src/it/csi/mddtools/servicegen/workflow/component/TrackingInvoker.java
@@ -0,0 +1,50 @@
+package it.csi.mddtools.servicegen.workflow.component;
+
+import java.util.Properties;
+
+import mddtools.usagetracking.ProfilingPacketBuilder;
+import mddtools.usagetracking.TrackingSender;
+
+import org.openarchitectureware.workflow.WorkflowContext;
+import org.openarchitectureware.workflow.issues.Issues;
+import org.openarchitectureware.workflow.monitor.ProgressMonitor;
+
+public class TrackingInvoker extends org.openarchitectureware.workflow.lib.AbstractWorkflowComponent{
+
+	public void checkConfiguration(Issues issues) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void invoke(WorkflowContext ctx, ProgressMonitor monitor,
+			Issues issues) {
+		// TODO Auto-generated method stub
+		System.out.println("invoke TrackingInvoker");
+	}
+
+	public void setTrack(boolean b){
+		System.out.println("setTrack:"+modelName);
+		Properties info = ProfilingPacketBuilder.packGenerateInfo("servicegen", "1.1.0.001", modelName, "?", targetProjectName);
+		System.out.println("333");
+		TrackingSender.sendTrackingInfo(info);
+	}
+	
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setTargetProjectName(String targetProjectName) {
+		this.targetProjectName = targetProjectName;
+	}
+
+	public String getTargetProjectName() {
+		return targetProjectName;
+	}
+
+	private String modelName;
+	private String targetProjectName;
+}
