21bd0b309dbf1f96247567833906ac6926a6b32f
PARODI Claudio 1759 <claudio.parodi@csi.it>
2013-10-18 12:13:09 +0000

    aggiunto generated not
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/servicegen/trunk@5439 be04b910-fd5c-0410-891f-bca81b977169

--- src/it/csi/mddtools/servicedef/provider/WSFADBindingItemProvider.java
+++ src/it/csi/mddtools/servicedef/provider/WSFADBindingItemProvider.java
@@ -163,7 +163,7 @@ public class WSFADBindingItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
